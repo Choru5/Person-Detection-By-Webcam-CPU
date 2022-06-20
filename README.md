d1edf5a92eab69999186a7f1bb0b9ea38722056b
Choru5 <98558514+Choru5@users.noreply.github.com>
2022-06-20 13:10:20 +1200

    init

--- README.md
+++ README.md
@@ -1,37 +1,31 @@
-# Person/Pet-Detection-by-Webcam
+# Person-Detection-by-Webcam
 
 Requirements:
- - A PC with a Nvidia GPU
+ - A PC (no GPU required)
  - A smartphone / webcam
  
  What it is:
- -  This is a machine learning model that has been trained to detect people / pets and notify you.
+ -  This is a machine learning model that has been trained to detect people and notify you.
  
  Installation Instructions:
  
  - Download this repository.
  - Make sure you have Python 3.8 installed
- - Install anaconda (https://www.anaconda.com/products/distribution) on your computer.
- - Open anaconda as administrator and cd to the directory of the persondetector.py script in this repository.
- - Install pytorch and cuda (https://pytorch.org/get-started)
- - Create a virtual environment using this command ``` conda create -n pytorch-gpu python==3.8 ```
- - Activate the virtual env using ``` conda activate pytorch-gpu ```
+ - Open cmd and cd into this repository.
  - Install required packages by running ``` pip install -r requirements.txt ```
  - Download DroidCam Client on your PC and the DroidCam app on your phone (if using a webcam ignore this)
 
  Customizable Settings:
  ```
+ notifTitle = "Warning" # This will change the title of the notification being displayed
+ notifContent = "Person detected!" # This will change the content of the notification being displayed
+
  visuals = True  # This will toggle the visiblity of the camera feed
  
  IN = 1  # This will toggle the webcam source (0 = normal webcam, 1 = smartphone DroidCam webcam)
- 
- target = 'person'  # This changes what you are detecting, examples are 'cat', 'dog', or 'person' by default 
- 
- modelFile = "yolov5s.pt"  # This is the AI model file, do not change this unless you really know what you are doing!
 ```
 
  How to use:
  
- - Open anaconda as administrator and cd to the directory of the persondetector.py script in this repository.
- - Run the command ``` conda activate pytorch-gpu ``` to start up the virtual environment
+ - Open cmd and cd into this repository.
  - Run the command ``` python pythondetector.py ``` to start the program, make sure you have DroidCam Client open and are connected to your phones DroidCam via Wi-Fi or USB (if using a webcam ignore this)
---
+++ persondetectorcpu.py
@@ -0,0 +1,89 @@
+import cv2
+import time
+import mediapipe as mp
+import numpy as np
+from plyer import notification
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+
+notifTitle = "Warning"  # This will change the title of the notification being displayed
+notifContent = "Person detected!"  # This will change the content of the notification being displayed
+
+visuals = True  # This will toggle the visiblity of the camera feed
+
+IN = 1  # This will toggle the webcam source (0 = normal webcam, 1 = smartphone DroidCam webcam)
+
+### ---------------------------------------------- Find person function -----------------------------------------------------
+def getPerson(checkimage):
+
+    personDetected = False
+
+    checkimage.flags.writeable = False
+    checkimage = cv2.cvtColor(checkimage, cv2.COLOR_BGR2RGB)
+    results = pose.process(checkimage)
+    checkimage.flags.writeable = True
+    checkimage = cv2.cvtColor(checkimage, cv2.COLOR_RGB2BGR)
+
+    if results.pose_landmarks:
+        personDetected = True
+        mp_drawing.draw_landmarks(checkimage, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, mp_drawing_styles.get_default_pose_landmarks_style() )
+                    
+        return personDetected, checkimage 
+
+    elif not results.pose_landmarks:
+        return personDetected, checkimage
+
+### ---------------------------------------------- Notify function -----------------------------------------------------
+def notifyMe(title, message):
+
+    notification.notify(
+        title = title,
+        message = message,
+        app_icon = "warning.ico", 
+        timeout = 10,
+    )
+
+### ---------------------------------------------- Main function -----------------------------------------------------
+def useWebcam(IN=None):
+    canNotify = True
+
+    cap = cv2.VideoCapture(IN)
+
+    pTime = 0
+    while cap.isOpened():
+        success, img = cap.read()
+
+        if not success:
+            print("No image found.")
+            continue
+
+        personDetected, displayImg = getPerson(img)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+ 
+        cv2.putText(displayImg, str(int(fps)), (5, 20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
+
+        if personDetected == True and canNotify == True:
+            notifyMe(notifTitle, notifContent)
+            canNotify = False
+
+        elif personDetected == False and canNotify == False:
+            canNotify = True         
+
+        if visuals == True:
+            cv2.imshow("capture", displayImg) 
+
+        cv2.waitKey(1)
+
+### -------------------  calling the main function-------------------------------
+
+useWebcam(IN)
+
+
+
+
